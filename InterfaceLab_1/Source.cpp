9aae976205c1f8b84c14d202b7acc31714378894
Darkest-White <darkest.white.fox@gmail.com>
2024-04-08 07:05:13 +0300

    Last version

--- InterfaceLab_1/Source.cpp
+++ InterfaceLab_1/Source.cpp
@@ -11,7 +11,7 @@ using namespace chrono;
 void random_rect(float radius, RectangleShape* rectangle) {
 	std::random_device rd;
 	std::mt19937 gen(rd());
-	std::uniform_real_distribution<float> dis(0.f, 2.f * 3.14);
+	std::uniform_real_distribution<float> dis(0, 2 * 3.14);
 
 	float angle = dis(gen);
 	float x = radius * cos(angle);
@@ -25,7 +25,15 @@ int main()
 	system("chcp 1251");
 	system("cls");
 
-	RenderWindow window(VideoMode(800, 700), "Lab 1", Style::Close);
+	int choice;
+	do
+	{
+		cout << "Эксперименты: 1, 2, 3" << endl;
+		cout << "Введите номер эксперимента: ";
+		cin >> choice;
+	} while (choice < 1 or choice > 3);
+
+	RenderWindow window(VideoMode(800, 700), "Lab1", Style::Close);
 
 	auto last_click_time = steady_clock::now();
 
@@ -35,42 +43,48 @@ int main()
 	bool IsClicked = 0;
 	bool IsMoved = 0;
 
-	float radius = 400;
+	float radius = 200;
 	float size = 8;
-	RectangleShape rectangle(Vector2f(2 * size, size));
+	sf::RectangleShape rectangle(sf::Vector2f(2 * size, size));
 	rectangle.setFillColor(Color(128, 128, 128));
 
 	sf::CircleShape circle(radius);
 	circle.setPointCount(200);
-	circle.setFillColor(Color::Transparent);
+	circle.setFillColor(sf::Color::Transparent);
 	circle.setOutlineThickness(1);
-	circle.setOutlineColor(Color::Red);
-	circle.setPosition(0, 0);
+	circle.setOutlineColor(sf::Color::Red);
+	circle.setPosition(-radius, -radius);
 
 	vector<float> dists = { 1, 20, 40, 60, 100, 150, 200, 250, 300, 350 };
+	vector<float> sizes = { 8, 10, 12, 15, 20, 30 , 40, 50, 70, 100 };
 
-	int current_dist = 0;
-	int current_try = 0;
+	int cur_dist = 0;
+	int cur_size = 0;
+	int cur_try = 0;
 
 	int tries = 5;
 
 	vector<float> med_nums;
 	vector<vector<float>> med_nums2;
-	med_nums.push_back(0);
+	med_nums.push_back(0.f);
 	med_nums2.push_back(*new vector<float>);
-	med_nums2.at(0).push_back(0);
+	med_nums2.at(0).push_back(0.f);
+
+
+	if (choice == 3) tries = 3;
 
 	while (window.isOpen())
 	{
 		Event event;
 		while (window.pollEvent(event))
 		{
-			if (event.type == Event::Closed)
+			if (event.type == Event::Closed or choice > 3 or choice < 1)
 				window.close();
-			else if (current_dist >= dists.size())
+			else if (cur_dist >= dists.size() and (choice == 1 or choice == 3))
 				window.close();
-			else if (event.type == Event::KeyPressed and event.key.code == Keyboard::Space and not IsClicked and not IsMoved)
-			{
+			else if (cur_size >= sizes.size() and choice == 2)
+				window.close();
+			else if (event.type == Event::KeyPressed and event.key.code == Keyboard::Space and not IsClicked and not IsMoved) {
 				IsClicked = 1;
 
 				pos = window.getPosition();
@@ -78,14 +92,19 @@ int main()
 				pos.x += 8;
 				Mouse::setPosition(pos);
 
-				radius = dists.at(current_dist);
-				circle.setRadius(radius);
-				circle.setPosition(-radius, -radius);
+				if (choice == 1 or choice == 3) {
+					radius = dists.at(cur_dist);
+					circle.setRadius(radius);
+					circle.setPosition(-radius, -radius);
+				}
+				if (choice == 2 or choice == 3) {
+					size = sizes.at(cur_size);
+					rectangle.setSize({ 2 * size, size });
+				}
 
 				random_rect(radius, &rectangle);
 			}
-			else if (IsClicked and Mouse::getPosition() != pos and not IsMoved) 
-			{
+			else if (IsClicked and Mouse::getPosition() != pos and not IsMoved) {
 				IsMoved = 1;
 
 				last_click_time = steady_clock::now();
@@ -99,50 +118,102 @@ int main()
 
 				last_click_time = current_time;
 
-				if (current_try < tries - 1)
-				{
-					med_nums.at(current_dist) += static_cast<float>(time_since_last_click) / 1e9;
-					current_try++;
+				if (cur_try < tries - 1) {
+					if (choice == 1) med_nums.at(cur_dist) += static_cast<float>(time_since_last_click) / 1e9;
+					else if (choice == 2) med_nums.at(cur_size) += static_cast<float>(time_since_last_click) / 1e9;
+					else if (choice == 3) med_nums2.at(cur_dist).at(cur_size) += static_cast<float>(time_since_last_click) / 1e9;
+					cur_try++;
 				}
-				else
-				{
-					med_nums.push_back(0);
-					med_nums.at(current_dist) += static_cast<float>(time_since_last_click) / 1e9;
-					med_nums.at(current_dist) /= 5;
-					current_dist++;
-					current_try = 0;
+				else {
+					if (choice == 1) {
+						med_nums.push_back(0);
+						med_nums.at(cur_dist) += static_cast<float>(time_since_last_click) / 1e9;
+						med_nums.at(cur_dist) /= 5;
+						cur_dist++;
+					}
+					else if (choice == 2) {
+						med_nums.push_back(0);
+						med_nums.at(cur_size) += static_cast<float>(time_since_last_click) / 1e9;
+						med_nums.at(cur_size) /= 5;
+						cur_size++;
+					}
+					else if (choice == 3) {
+						med_nums2.at(cur_dist).at(cur_size) += static_cast<float>(time_since_last_click) / 1e9;
+						med_nums2.at(cur_dist).at(cur_size) /= tries;
+						if (cur_size < sizes.size() - 1) {
+							cur_size++;
+						}
+						else {
+							cur_size = 0;
+							cur_dist++;
+							med_nums2.push_back(*new vector<float>);
+						}
+						med_nums2.at(cur_dist).push_back(0);
+					}
+					cur_try = 0;
 				}
+
 				IsClicked = 0;
 				IsMoved = 0;
 			}
 		}
 		window.clear(Color::Black);
-		if (IsClicked)
-		{
+		if (IsClicked) {
 			window.draw(rectangle);
 			window.draw(circle);
 		}
 		window.display();
 	}
+	if (choice == 1)
+	{
+		cout << "Средние значения времени между нажатиями:" << endl;
+		for (int i = 0; i < dists.size(); i++)
+			cout << "Для значения " << dists[i] << ": " << med_nums[i] << endl;
+	}
+	else if (choice == 2)
+	{
+		cout << "Средние значения времени между нажатиями:" << endl;
+		for (int i = 0; i < sizes.size(); i++)
+			cout << "Для значения " << sizes[i] << ": " << med_nums[i] << endl;
 
-	cout << "Средние значения времени между нажатиями:" << endl;
-	for (int i = 0; i < dists.size(); i++)
-		cout << "Для значения " << dists[i] << ": " << med_nums[i] << endl;
-
+	}
+	else if (choice == 3)
+	{
+		cout << "Средние значения времени между нажатиями:" << endl;
+		for (int i = 0; i < med_nums2.size(); i++)
+		{
+			for (int j = 0; j < med_nums2[i].size(); j++)
+			{
+				cout << "Для расстояния " << dists[i] << " и размера " << sizes[j] << ": " << med_nums2[i][j] << endl;
+			}
+		}
+	}
 	ofstream outputFile("average_times.txt", ios::app);
 
 	if (outputFile.is_open())
 	{
-		outputFile << "Номер эксперимента: " << 1 << endl;
+		outputFile << "Номер эксперимента: " << choice << endl;
 		outputFile << "Средние значения времени между нажатиями:" << endl;
 
 		// Запись средних значений в файл
-		for (int i = 0; i < dists.size(); i++)
-			outputFile << "Для значения " << dists[i] << ": " << med_nums[i] << endl;
+		if (choice == 1)
+			for (int i = 0; i < dists.size(); i++)
+				outputFile << "Для значения " << dists[i] << ": " << med_nums[i] << endl;
+		else if (choice == 2)
+			for (int i = 0; i < sizes.size(); i++)
+				outputFile << "Для значения " << sizes[i] << ": " << med_nums[i] << endl;
+		else if (choice == 3)
+			for (int i = 0; i < med_nums2.size(); i++)
+			{
+				for (int j = 0; j < med_nums2[i].size(); j++)
+				{
+					outputFile << "Для расстояния " << dists[i] << " и размера " << sizes[j] << ": " << med_nums2[i][j] << endl;
+				}
+			}
 
 		outputFile.close();
 
-		cout << "Данные по эксперименту " << 1 << " были добавлены в файл 'average_times.txt'" << endl;
+		cout << "Данные по эксперименту " << choice << " были добавлены в файл 'average_times.txt'" << endl;
 	}
 	else
 	{
